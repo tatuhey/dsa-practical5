a0bff5b7f02aa6dae7c6081618402bb24bb56779
tatuhey <17900375@student.curtin.edu.au>
2026-09-16 22:58:41 +0800

    displaying and autoadding

--- main.java
+++ main.java
@@ -52,11 +52,35 @@ public class main {
     }
 
     public static void displaying(Scanner sc, DSABinarySearchTree bSearchTree) {
+        int sel = 10000;
+        while(sel != 0) {
+            System.out.println("1. In-order traversal\n2. Pre-order traversal\n3. Post-order traversal");
 
+            sel = sc.nextInt();
+            switch(sel) {
+                case 1:
+                    bSearchTree.inOrder();
+                    break;
+                case 2:
+                    bSearchTree.preOrder();
+                    break;
+                case 3:
+                    bSearchTree.postOrder();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Wrong selection");
+            }
+        }
     }
 
     public static void autoadding(Scanner sc, DSABinarySearchTree bSearchTree) {
-
+        System.out.println("Put number of node: ");
+        int input = sc.nextInt();
+        for(int i = 0; i < input; i++) {
+            bSearchTree.insert("node" + i+1, i+1);
+        }
     }
 }
 
