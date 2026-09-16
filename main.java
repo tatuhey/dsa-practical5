ac32304dee014e164dcbe3be78a8adb5125e6526
tatuhey <17900375@student.curtin.edu.au>
2026-09-16 22:49:58 +0800

    addnode and removenode in main is done

--- main.java
+++ main.java
@@ -1,7 +1,61 @@
-
+import java.util.Scanner;
 public class main {
 
     public static void main(String[] args ) {
+        int selection = 10000;
+        Scanner sc = new Scanner(System.in);
+
+        DSABinarySearchTree bSearchTree = new DSABinarySearchTree();
+
+        while(selection != 0) {
+            System.out.println("Select testing:");
+            System.out.println("1. Add node\n2. Delete node\n3. Display tree\n4. Add node automatically");
+
+            selection = sc.nextInt();
+            switch(selection) {
+                case 1:
+                    adding(sc, bSearchTree);
+                    break;
+                case 2:
+                    deleting(sc, bSearchTree);
+                    break;
+                case 3:
+                    displaying(sc, bSearchTree);
+                    break;
+                case 4:
+                    autoadding(sc, bSearchTree);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Wrong selection");
+            }
+        }
+    
+        sc.close();
+    }
+
+    public static void adding(Scanner sc, DSABinarySearchTree bSearchTree) {
+        System.out.println("Add a node below. Format is <key,value>");
+        String input;
+        String regex = "[,\\.\\s]";
+        input = sc.nextLine();
+        String[] arr = input.split(regex);
+        bSearchTree.insert(arr[0], arr[1]);
+    }
+
+    public static void deleting(Scanner sc, DSABinarySearchTree bSearchTree) {
+        System.out.println("Remove a node below. Format is <key>");
+        String input;
+        input = sc.nextLine();
+        bSearchTree.delete(input);
+    }
+
+    public static void displaying(Scanner sc, DSABinarySearchTree bSearchTree) {
+
+    }
+
+    public static void autoadding(Scanner sc, DSABinarySearchTree bSearchTree) {
 
     }
 }
